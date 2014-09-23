809da35926dc19ee0bec5e3e66cf25c79b01274c
Grégoire Delattre <gregoire.delattre@gmail.com>
2014-09-23 11:31:01 +0200

    Better offlineimap logging

--- .mutt/mailrun.sh
+++ .mutt/mailrun.sh
@@ -5,6 +5,6 @@ export LANG=C
 LOGFILE=${HOME}/offlineimap.log
 
 echo `date` "Starting" >> ${LOGFILE}
-/usr/bin/flock --exclusive --timeout 5 /tmp/offlineimap.lock -c /usr/bin/offlineimap >> ${LOGFILE}
+/usr/bin/flock --exclusive --timeout 5 /tmp/offlineimap.lock -c /usr/bin/offlineimap 2>&1 >> ${LOGFILE} || echo `date` "Locked" >> ${LOGFILE}
 echo `date` "Done" >> ${LOGFILE}
 echo >> ${LOGFILE}
