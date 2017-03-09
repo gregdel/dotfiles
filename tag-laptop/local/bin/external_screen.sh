38e0a8013da5d9ddbfaaf5df6f25f6721fcaaeae
Grégoire Delattre <gregoire.delattre@gmail.com>
2017-03-09 23:26:28 +0100

    [awesome] Apply shellcheck on external_screens.sh

--- tag-laptop/local/bin/external_screen.sh
+++ tag-laptop/local/bin/external_screen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SCREEN_COUNT=$(xrandr | grep -w connected | wc -l)
+SCREEN_COUNT=$(xrandr | grep -w connected -c)
 echo "Number of screens connected: ${SCREEN_COUNT}"
 
 xrandr --output VIRTUAL1 --off
@@ -8,7 +8,7 @@ xrandr --output HDMI2 --off
 xrandr --output HDMI1 --off
 xrandr --output DP2 --off
 
-if [ $SCREEN_COUNT -ge 2 ]; then
+if [ "$SCREEN_COUNT" -ge 2 ]; then
     xrandr --output eDP1 --primary --mode 3200x1800 --pos 4032x0
     xrandr --output DP1 --mode 1920x1080 --pos 0x0 --dpi 96 --scale 2.1x2.1
 else
@@ -16,4 +16,5 @@ else
     xrandr --output eDP1 --primary --mode 3200x1800 --pos 0x0
 fi
 
-source ${HOME}/.xinitrc
+# shellcheck disable=1090
+source "$HOME/.xinitrc"
