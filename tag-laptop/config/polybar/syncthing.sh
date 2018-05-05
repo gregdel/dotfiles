f40008a3ebe58eae2a7f84c80b058442491e0b22
Grégoire Delattre <gregoire.delattre@gmail.com>
2018-05-05 13:04:56 +0200

    polybar: add syncthing

---
+++ tag-laptop/config/polybar/syncthing.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+_help() {
+	echo "$0 [start|stop|status|toggle]"
+	exit 1
+}
+
+_status() {
+	if systemctl --user is-active syncthing >/dev/null; then
+		echo ""
+		return 0
+	else
+		echo ""
+		return 1
+	fi
+}
+
+_open() {
+	xdg-open http://localhost:8384
+}
+
+_start() {
+	systemctl --user start syncthing
+}
+
+_stop() {
+	systemctl --user stop syncthing
+}
+
+_toggle() {
+	if _status >/dev/null; then
+		_stop
+	else
+		_start
+	fi
+}
+
+case "$1" in
+	status)  _status ;;
+	start)   _start  ;;
+	stop)    _stop   ;;
+	toggle)  _toggle ;;
+	open)    _open   ;;
+	*)       _help   ;;
+esac
