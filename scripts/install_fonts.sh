cd0a27fc9e14e6512d443bf1324a58072c66d58d
Grégoire Delattre <gregoire.delattre@gmail.com>
2018-05-05 13:06:31 +0200

    fonts: update nerdfonts

--- scripts/install_fonts.sh
+++ scripts/install_fonts.sh
@@ -1,12 +1,15 @@
-#!/bin/bash
+#!/bin/sh
+
+set -e
 
 FONT_DIRECTORY=~/.local/share/fonts
 
-FONT_VERSION="1.0.0"
+
+FONT_VERSION="2.0.0"
 FONTS_URL="https://raw.githubusercontent.com/ryanoasis/nerd-fonts/$FONT_VERSION/patched-fonts"
 FONTS="
-    DejaVuSansMono/Regular/complete/DejaVu%20Sans%20Mono%20for%20Powerline%20Nerd%20Font%20Complete%20Mono.ttf
-    Inconsolata/complete/Inconsolata%20for%20Powerline%20Nerd%20Font%20Complete.otf
+	DejaVuSansMono/Regular/complete/DejaVu%20Sans%20Mono%20Nerd%20Font%20Complete%20Mono.ttf
+	Inconsolata/complete/Inconsolata%20Nerd%20Font%20Complete.otf
 "
 
 if [ -d $FONT_DIRECTORY ]; then
@@ -15,7 +18,7 @@ if [ -d $FONT_DIRECTORY ]; then
 fi
 
 mkdir -p $FONT_DIRECTORY
-cd $FONT_DIRECTORY || exit 1
+cd $FONT_DIRECTORY
 
 echo "Downloading fonts"
 for font in $FONTS; do
