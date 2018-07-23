97fcdcbed66e657bf26d8f0c9d4e2b2131dfc147
Grégoire Delattre <gregoire.delattre@corp.ovh.com>
2018-07-23 11:53:45 +0200

    mutt: update the install script

--- scripts/install_neomutt.sh
+++ scripts/install_neomutt.sh
@@ -1,31 +1,20 @@
 #!/bin/bash
 
-VERSION="neomutt-20161126"
+VERSION="neomutt-20180716"
 URL="https://github.com/neomutt/neomutt.git"
-BUILD_DIR_ROOT=$HOME/dev/build
-BUILD_DIR=$BUILD_DIR_ROOT/neomutt
+BUILD_DIR_ROOT="$HOME/dev/build"
+BUILD_DIR="$BUILD_DIR_ROOT/neomutt"
 
-if [ ! -d "$BUILD_DIR_ROOT" ]; then
-    mkdir -p "$BUILD_DIR_ROOT"
-fi
-
-if [ ! -d "$BUILD_DIR" ]; then
-    git clone "$URL" "$BUILD_DIR"
-    cd neomutt || exit 1
-fi
+[ ! -d "$BUILD_DIR_ROOT" ] && mkdir -p "$BUILD_DIR_ROOT"
+[ ! -d "$BUILD_DIR" ] && git clone "$URL" "$BUILD_DIR"
 
 cd "$BUILD_DIR" || exit 1
 git pull origin master --tags
 git checkout "$VERSION"
 
-# Install dependencies
-sudo apt update
-sudo apt install -y libssl-dev xsltproc libncursesw6-dev libtokyocabinet-dev
-
 # Configure and make
-autoreconf --install
-./configure --enable-sidebar --enable-smtp --with-ssl --with-sasl --enable-hcache --with-tokyocabinet
+./configure \
+	--disable-doc \
+	--tokyocabinet \
+	--ssl
 make
-
-# Make install
-sudo make install
