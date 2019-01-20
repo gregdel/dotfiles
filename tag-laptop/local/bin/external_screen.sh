5cdd89f90b216b8b2e270ed8592895d30f2ed80d
Grégoire Delattre <gregoire.delattre@gmail.com>
2019-01-20 16:51:13 +0100

    laptop: add systemd user service for graphical apps

--- tag-laptop/local/bin/external_screen.sh
+++ tag-laptop/local/bin/external_screen.sh
@@ -1,28 +1,25 @@
 #!/bin/sh
 
-export DISPLAY=:0
-SCREEN_COUNT=$(xrandr | grep -w connected -c)
-echo "Number of screens connected: $SCREEN_COUNT"
+DOCK_DEVICE=/sys/devices/pci0000:00/0000:00:1c.0/0000:01:00.0/device
 
-if [ "$SCREEN_COUNT" -ge 2 ]; then
-    xrandr \
-        --output eDP1 --primary --mode 3200x1800 --pos 4032x0 --rotate normal --dpi 166 \
-        --output DP1 --mode 1920x1080 --pos 0x0 --rotate normal --scale 2x2 --auto \
-        --output DP2 --off \
-        --output VIRTUAL1 --off
+if [ -f "$DOCK_DEVICE" ]; then
+	xrandr \
+		--output eDP1 --primary --mode 3200x1800 --pos 4032x0 --rotate normal --dpi 166 \
+		--output DP1 --mode 1920x1080 --pos 0x0 --rotate normal --scale 2x2 --auto \
+		--output DP2 --off \
+		--output VIRTUAL1 --off
+
+	systemctl --user start polybar-external.service
+	systemctl --user reload i3
+	notify-send -i display "Monitor" "External monitor connected"
 else
-    xrandr \
-        --output eDP1 --primary --mode 3200x1800 --pos 0x0 --rotate normal --dpi 166 \
-        --output DP1 --off \
-        --output DP2 --off \
-        --output VIRTUAL1 --off
+	xrandr \
+		--output eDP1 --primary --mode 3200x1800 --pos 0x0 --rotate normal --dpi 166 \
+		--output DP1 --off \
+		--output DP2 --off \
+		--output VIRTUAL1 --off
+
+	systemctl --user stop polybar-external.service
+	systemctl --user reload i3
+	notify-send -i display "Monitor" "External monitor disconnected"
 fi
-
-# Restart i3
-i3-msg restart
-
-# Add a random wallpaper
-feh --randomize --bg-fill "$HOME/pictures/Wallpapers"
-
-# shellcheck disable=1091
-. /home/greg/.xinitrc
---
+++ tag-laptop/local/bin/graphical-services.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+systemctl --user start polybar
+systemctl --user start compton
+systemctl --user start dunst
+systemctl --user start wallpaper
