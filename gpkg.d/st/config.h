e72815812b4c3fc6efe35ca09d4a93055e316728
Grégoire Delattre <gregoire.delattre@gmail.com>
2019-02-05 14:33:45 +0100

    bluetooth: add the bluetooth status in polybar

--- gpkg.d/st/config.h
+++ gpkg.d/st/config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "DejaVuSansMono Nerd Font Mono:style=Book:size=12:antialias=true:autohint=true";
-static int borderpx = 2;
+static int borderpx = 10;
 
 /*
  * What program is execed by st depends of these precedence rules:
